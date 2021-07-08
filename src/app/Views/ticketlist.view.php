12b64309b94b56f90d1a867cc3eb910fec565c7c
Nalis1000 <nico@blatters.net>
2021-07-08 10:55:57 +0200

    Ticklist.view.php update

--- src/app/Views/ticketlist.view.php
+++ src/app/Views/ticketlist.view.php
@@ -5,16 +5,24 @@
     <title>New Buy Order</title>
     <!-- Set base for relative urls to the directory of index.php: -->
     <base href="<?= ROOT_URL ?>/">
+    <link href="src/public/css/bootstrap-css/bootstrap.css">
+    <script href="src/public/js/bootstrap-js/bootstrap.bundle.js"></script>
 </head>
 <body>
     <table>
         <thead>>
         <tr><th></th><th>Ticket</th><th>Action</th></tr>
         </thead>
+        <tbody>
         <?php foreach($tickets as $ticket){ ?>
+        <tr><td><input type="checkbox" id="paid" name="paid"value="<?php $ticket['orderid']?>"></td>
+            <button data-toggle="collapse" data-target="#<?php $ticket['orderid']?>" id="ticket"><?='Name: ' .e($ticket['lastname']." ".e($tickets['firstname']).
+                'Concert: ' .e($ticket['artist']);?></button>
 
+            <td></td></tr>
         <?php } ?>
-        <td><button id="add">Add</button></td>
+        </tbody>
+        <<td><button id="add">Add</button></td>
     </table>
 
 
